DecodeGenetics/BamQC

commit 9b5f288e68ff526ba2b1acb7aabf2b2c19202769

Moving code for determining lane to functions.

src/bamqualcheck.cpp
@@ -328,6 +328,72 @@ void print(TString & str, std::ofstream & outFile){
 
 void RunBamStream(std::vector<ReadQualityHasher> & sps, seqan::CharString & seq, seqan::CharString & qual, size_t & qsize, size_t & ksize);
 
+// -----------------------------------------------------------------------------
+// FUNCTION getSampleIdAndLaneNames()
+// -----------------------------------------------------------------------------
+
+void getSampleIdAndLaneNames(seqan::CharString & id, std::map<seqan::CharString, int> & laneNames, seqan::BamHeader & header)
+{
+    seqan::CharString lanename;
+    for (unsigned i = 0; i < length(header.records); ++i)
+    {
+        if (header.records[i].type == seqan::BAM_HEADER_READ_GROUP)
+        {
+            for (unsigned j = 0; j < length(header.records[i].tags); ++j)
+            {
+                if (seqan::getValueI1(header.records[i].tags[j]) == "ID")
+                {
+                    lanename = seqan::getValueI2(header.records[i].tags[j]);
+                    laneNames[lanename] = laneNames.size();
+                }
+                if (seqan::getValueI1(header.records[i].tags[j]) == "SM")
+                {
+                    id = seqan::getValueI2(header.records[i].tags[j]);
+                }
+            }
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+// FUNCTION getLane()
+// -----------------------------------------------------------------------------
+
+unsigned getLane(seqan::BamAlignmentRecord & record,
+                 seqan::BamTagsDict & tagsDict,
+                 std::map<seqan::CharString, int> & laneNames,
+                 seqan::BamIOContext<seqan::StringSet<seqan::CharString> > & context)
+{
+    unsigned l;
+    for (unsigned tagid = 0; tagid < length(tagsDict); ++tagid)
+    {
+        if (getTagKey(tagsDict, tagid) == "RG")
+        {
+            if (getTagType(tagsDict, tagid) == 'Z')
+            {
+                seqan::CharString readlane;
+                readlane = getTagValue(tagsDict, tagid);
+                readlane = infix(readlane, 1, length(readlane)-1);
+                l = laneNames[readlane];
+                break;
+            }
+            else
+            {
+                std::cout << "Read does not have Z" << "\n";
+                if (write2(std::cout, record, context, seqan::Sam()) != 0)
+                {
+                    std::cerr << "ERROR: Could not write record to stdout \n";
+                }
+                return 1;
+            }
+        }
+    }
+}
+
+// =============================================================================
+// *** MAIN ***
+// =============================================================================
+
 int main(int argc, char const ** argv)
 {
 
@@ -380,30 +446,11 @@ int main(int argc, char const ** argv)
         }
     }
 
-    //Check if more then one lane.
-    unsigned lanecount = 0;
-    seqan::CharString lanename;
-    seqan::CharString pn;
+    //Check if more than one lane.
+    seqan::CharString sampleId;
     std::map<seqan::CharString, int> laneNames;
-    for (unsigned i = 0; i < length(header.records); ++i)
-    {
-        if (header.records[i].type == seqan::BAM_HEADER_READ_GROUP)
-        {
-            for (unsigned j = 0; j < length(header.records[i].tags); ++j)
-            {
-                if (seqan::getValueI1(header.records[i].tags[j]) == "ID")
-                {
-                    lanename = seqan::getValueI2(header.records[i].tags[j]);
-                    laneNames[lanename] = lanecount;
-                    lanecount +=1;
-                }
-                if (seqan::getValueI1(header.records[i].tags[j]) == "SM")
-                {
-                    pn = seqan::getValueI2(header.records[i].tags[j]);
-                }
-            }
-        }
-    }
+    getSampleIdAndLaneNames(sampleId, laneNames, header);
+    unsigned lanecount = laneNames.size();
 
     seqan::clear(header);
 
@@ -454,47 +501,22 @@ int main(int argc, char const ** argv)
             return 1;
         }
         seqan::BamTagsDict tagsDict(record.tags);
-        unsigned l;
-        for (unsigned tagid = 0; tagid < length(tagsDict); ++tagid)
-        {
-            if (getTagKey(tagsDict, tagid) == "RG")
-            {
-                if (getTagType(tagsDict, tagid) == 'Z')
-                {
-                    seqan::CharString readlane;
-                    readlane = getTagValue(tagsDict, tagid);
-                    readlane = infix(readlane, 1, length(readlane)-1);
-                    l = laneNames[readlane];
-                    break;
-                }
-                else
-                {
-                    std::cout << "Read does not have Z" << "\n";
-                    if (write2(std::cout, record, context, seqan::Sam()) != 0)
-                    {
-                        std::cerr << "ERROR: Could not write record to stdout \n";
-                    }
-                    return 1;
-                }
-            }
-        }
+        unsigned l = getLane(record, tagsDict, laneNames, context);
 
-        // Check the four highest flags and continue if any is set.
+        // Check the four highest flags and continue in some cases.
         if (hasFlagSupplementary(record)) {
             rall[l].supplementary +=1;
-            continue;
-        }
-        if (hasFlagDuplicate(record)){
-            rall[l].duplicates +=1;
-            continue;
-        }
-        if (hasFlagQCNoPass(record)){
-            rall[l].QCfailed +=1;
             continue;
         }
         if (hasFlagSecondary(record)){
             rall[l].not_primary_alignment +=1;
             continue;
+        }
+        if (hasFlagDuplicate(record)){
+            rall[l].duplicates +=1;
+        }
+        if (hasFlagQCNoPass(record)){
+            rall[l].QCfailed +=1;
         }
 
         // Triplet counting.
@@ -502,7 +524,7 @@ int main(int argc, char const ** argv)
             return 1;
         }
 
-        if (hasFlagRC(record)) // check if read is reversed complemented
+        if (hasFlagRC(record)) // check if read is in reverse complement
         {
             reverseComplement(record.seq);
             reverse(record.qual);
@@ -582,7 +604,7 @@ int main(int argc, char const ** argv)
 
     for (std::map<seqan::CharString,int>::iterator it=laneNames.begin(); it!=laneNames.end(); ++it)
     {
-            outFile << "pn " << pn << std::endl;
+            outFile << "sample_id " << sampleId << std::endl;
             outFile << "lane " << it->first << std::endl;
             int lid = it->second;
 

commit 752732825a36bc6a281d55f25b2a60b3aeda2ce8

Fixing error return value in functions introduced in 9b5f288e68ff526ba2b1acb7aabf2b2c19202769.

src/bamqualcheck.cpp
@@ -384,7 +384,7 @@ unsigned getLane(seqan::BamAlignmentRecord & record,
                 {
                     std::cerr << "ERROR: Could not write record to stdout \n";
                 }
-                return 1;
+                return -1;
             }
         }
     }
@@ -396,6 +396,10 @@ unsigned getLane(seqan::BamAlignmentRecord & record,
 
 int main(int argc, char const ** argv)
 {
+    // Typedefs for name store, cache, and BAM I/O context.
+    typedef seqan::StringSet<seqan::CharString> TNameStore;
+    typedef seqan::NameStoreCache<TNameStore>   TNameStoreCache;
+    typedef seqan::BamIOContext<TNameStore>     TBamIOContext;
 
     ProgramOptions opt;
     seqan::ArgumentParser::ParseResult res = parseCommandLine(opt, argc, argv); // Parse the command line.
@@ -403,6 +407,7 @@ int main(int argc, char const ** argv)
     {
         return res == seqan::ArgumentParser::PARSE_ERROR;
     }
+
     seqan::Stream<seqan::Bgzf> inStream; // Open BGZF Stream for reading.
     if (!open(inStream, toCString(opt.bamFile), "r")) //check if toCstring is needed
     {
@@ -416,9 +421,6 @@ int main(int argc, char const ** argv)
         return 1;
     }
 
-    typedef seqan::StringSet<seqan::CharString> TNameStore; // Setup name store, cache, and BAM I/O context.
-    typedef seqan::NameStoreCache<TNameStore>   TNameStoreCache;
-    typedef seqan::BamIOContext<TNameStore>     TBamIOContext;
     TNameStore      nameStore;
     TNameStoreCache nameStoreCache(nameStore);
     TBamIOContext   context(nameStore, nameStoreCache);
@@ -502,6 +504,7 @@ int main(int argc, char const ** argv)
         }
         seqan::BamTagsDict tagsDict(record.tags);
         unsigned l = getLane(record, tagsDict, laneNames, context);
+        if (l == -1) return 1;
 
         // Check the four highest flags and continue in some cases.
         if (hasFlagSupplementary(record)) {